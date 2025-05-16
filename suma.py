b97c020b8018be7f3811c03e67a30e4f8cc1d9aa
Yakelin Romero <ysofiaromero365@gmail.com>
2025-05-15 21:01:17 -0500

    Modifiqué el archivo suma.py para mostrar más detalles

--- suma.py
+++ suma.py
@@ -1,1 +1,7 @@
-print(3 + 5)
+def sumar(a, b): 
+    return a + b
+
+n1 = int(input("Ingrese el primer número: "))
+n2 = int(input("Ingrese el segundo número: "))
+resultado = sumar(n1, n2)
+print(f"La suma es: {resultado}")
